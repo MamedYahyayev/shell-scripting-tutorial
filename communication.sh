996834e95e7fe8d238691fe8648a30881366a84a
Mamed Yahyayev <yahyayev2001@gmail.com>
2021-01-25 18:15:14 +0400

    Communication

---
+++ communication.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+# ping command - this command sends request to host or ip address
+# The ping command is useful for the following −
+#  1. Tracking and isolating hardware and software problems.
+#  2. Determining the status of the network and various foreign hosts.
+#  3. Testing, measuring, and managing networks.
+
+# if host doesn't exists , bash prints "unknows host <name>" message on the console
+
+# FTP (File Transfer Protocol) Utility
+# this protocol helps us upload and download files from one computer to another one.
+
+# we can connect the host with ftp and hostname
+# For example
+# ftp hostname
+
+# In this protocol , it has a few useful commands
+
+# put filename - this will send the file to the host
+# get filename - this will download the file from the host
+# mput file list - this allow us to upload more than one files to the host
+# mget file list - this allow us to download more than one files from the host
+# quit -- this command quits the current login
+
+
+# The finger Utility
+# The finger command displays information about users on a given host. The host can be # either local or remote.
+# For example finger @avtar.com
