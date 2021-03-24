01738fbf4860d0ce21353526e27db83d7235a098
Mamed Yahyayev <yahyayev2001@gmail.com>
2021-03-24 20:00:17 +0400

    Bash Functions

--- advance/functions.sh
+++ advance/functions.sh
@@ -52,3 +52,42 @@ function2(){
 }
 
 function1;
+
+
+# Function variables
+# In the bash script, all variables are by default global and can be used throughout the file
+# the following scenario, we create 2 v variables, one of them is global , the other one is local
+# we can declare local variable with local keyword  
+v="Global";
+
+printLocalVariable() {
+    local v="Local";
+    echo ${v};
+}
+
+printLocalVariable # result will be Local
+
+
+# delete function 
+# we can delete the function witr unset keyword
+# here is the syntax: unset <function_name> 
+deletedFunction() {
+    echo "This is the deleted function";
+}
+
+deletedFunction
+
+unset deletedFunction;
+
+# deletedFunction # result will be: deletedFunction: command not found
+
+# Concatenate 2 strings with bash functions 
+
+concatNameAndSurname() {
+    declare fullname;
+    fullname="$1 $2";
+    echo ${fullname};
+}
+
+fullname=$(concatNameAndSurname Samir Samirov);
+echo "Fullname is: ${fullname}";
